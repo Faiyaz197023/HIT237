6137b480660157e67868d84790506d58b6a97005
Abdurrahman <s371930@students.cdu.edu.au>
2025-04-16 18:30:48 +0930

    Final files

--- HIT237-master/data/disease_protection.py
+++ HIT237-master/data/disease_protection.py
@@ -21,7 +21,7 @@ protection_data = [
         "key": "mango_scale",
         "name": "Mango Scale",
         "image": "images/pink_wax_scale.jpeg",
-        "measure": "High volume (1200L/ha) cover sprays after pruning with mineral oils and methidathion depending on scale activity."
+        "measure": "High volume (1200L/ha) copper sprays after pruning with mineral oils and methidathion depending on scale activity."
     },
     {
         "key": "mango_mmd",
